03e4e5816c0c5e16bd4675f699b7392336532fb3
diego salgado <diegosb55@gmail.com>
2017-03-24 17:41:36 -0400

    class_roaster

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,6 @@
+class StaticController < ApplicationController
+
+  def hello
+    @year = Time.now.year
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'hello' => 'static#hello'
+  get 'users' => 'users#index'
+
+  get 'users/:id', to: "users#show"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
